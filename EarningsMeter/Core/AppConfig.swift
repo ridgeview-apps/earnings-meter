49b731a1cbd6a80fb3487e650784e2f585e5ab51
Shilan Patel <shilan.patel@gmail.com>
2022-05-03 21:22:25 +0100

    Bumped app version no: 1.5

--- EarningsMeter/Core/AppConfig.swift
+++ EarningsMeter/Core/AppConfig.swift
@@ -11,9 +11,19 @@ struct AppConfig {
 extension AppConfig {
     
     // Load config from build settings
-    static let real = AppConfig(contactUsEmail: BuildSettings.CONTACT_US_EMAIL,
-                                appStoreProductUrl: BuildSettings.APP_STORE_PRODUCT_URL,
-                                appCenter: .init(appSecret: BuildSettings.APPCENTER_APP_SECRET))
+    static let real: AppConfig = {
+        let infoPlistValues = Bundle.main.infoPlistValues(forKey: "appConfig")
+        
+        let appConfig = AppConfig(
+            contactUsEmail: infoPlistValues["contactUsEmail"],
+            appStoreProductUrl: infoPlistValues[url: "appStoreProductUrl"],
+            appCenter: .init(
+                appSecret: infoPlistValues["appCenterAppSecret"]
+            )
+        )
+        
+        return appConfig
+    }()
 
 }
 
@@ -37,3 +47,76 @@ extension AppConfig {
         let appSecret: String
     }
 }
+
+
+
+/*
+ 
+ import Foundation
+ import RidgeviewCore
+
+ struct AppConfig {
+     
+     let contactUsEmail: String
+     let appStoreProductUrl: URL
+     let appCenter: AppCenter
+     let transportAPI: TransportAPI
+ }
+
+ // MARK: - Real instance
+ extension AppConfig {
+     
+     // Load config from Main Info plist
+     
+     static let real: AppConfig = {
+         let infoPlistValues = Bundle.main.infoPlistValues(forKey: "appConfig")
+         
+         let appConfig = AppConfig(
+             contactUsEmail: infoPlistValues["contactUsEmail"],
+             appStoreProductUrl: infoPlistValues[url: "appStoreProductUrl"],
+             appCenter: .init(
+                 appSecret: infoPlistValues["appCenterAppSecret"]
+             ),
+             transportAPI: .init(
+                 baseURL: infoPlistValues[url: "transportAPIURL"],
+                 appId: infoPlistValues["transportAPIAppId"],
+                 appKey: infoPlistValues["transportAPIAppKey"]
+             )
+         )
+         
+         return appConfig
+     }()
+
+ }
+
+ // MARK: - Fake instance
+ #if DEBUG
+ extension AppConfig {
+     
+     static let fake = AppConfig(contactUsEmail: "test@notanemail.com",
+                                 appStoreProductUrl: .fake,
+                                 appCenter: .init(appSecret: "fakeAppCenterSecret"),
+                                 transportAPI: .init(baseURL: .fake,
+                                                     appId: "fakeAPIAppId",
+                                                     appKey: "fakeAPIAppKey"))
+ }
+ #endif
+
+ private extension URL {
+     static let fake = URL(string: "https://fakeurl.com")!
+ }
+
+ extension AppConfig {
+     
+     struct AppCenter {
+         let appSecret: String
+     }
+     
+     struct TransportAPI {
+         let baseURL: URL
+         let appId: String
+         let appKey: String
+     }
+ }
+
+ */
