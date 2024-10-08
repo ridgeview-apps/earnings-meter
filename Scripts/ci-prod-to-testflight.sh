e7546fb11c5403ba71b818853bc7bd739bdc580b
Shilan Patel <shilan.patel@gmail.com>
2024-10-08 14:53:32 +0100

    Update release notes and CI config

--- Scripts/ci-prod-to-testflight.sh
+++
@@ -1,8 +0,0 @@
-export XCODE_CONFIG=Prod
-export DISTRIBUTION_GROUPS='Beta Testers'
-
-bundle install
-bundle update fastlane
-# bundle exec fastlane unit_tests
-bundle exec fastlane build
-bundle exec fastlane distribute
--- Scripts/ci-unit-tests.sh
+++
@@ -1,7 +0,0 @@
-export XCODE_CONFIG=Debug
-
-cd $(dirname $0)/..
-
-bundle install
-bundle update fastlane
-bundle exec fastlane unit_tests
