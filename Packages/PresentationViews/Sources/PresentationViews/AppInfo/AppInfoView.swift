020e73e7cdc71ee6bd52dd5ea9ad53d461dadd6b
Shilan Patel <shilan.patel@gmail.com>
2026-06-08 16:33:39 +0100

    Fixed localized string

--- Packages/PresentationViews/Sources/PresentationViews/AppInfo/AppInfoView.swift
+++ Packages/PresentationViews/Sources/PresentationViews/AppInfo/AppInfoView.swift
@@ -64,7 +64,7 @@ public struct AppInfoView: View {
     }
 
     private var emailSubject: String {
-        String(localized: "contact.us.subject \(contactUs.appName)")
+        String(localized: .contactUsSubject(contactUs.appName))
     }
 
     private var emailBody: String {
